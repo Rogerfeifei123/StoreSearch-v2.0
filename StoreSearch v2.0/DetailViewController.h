4f352ec84e5564a3bd627fca8656e81a8d6feed7
Rogerfeifei123 <rogercina@163.com>
2016-12-04 21:25:41 +0800

    Wait a moment

---
+++ StoreSearch v2.0/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  StoreSearch v2.0
+//
+//  Created by ZhuRoger on 04/12/2016.
+//  Copyright © 2016 ZhuRoger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ StoreSearch v2.0/DetailViewController.m
@@ -0,0 +1,42 @@
+//
+//  DetailViewController.m
+//  StoreSearch v2.0
+//
+//  Created by ZhuRoger on 04/12/2016.
+//  Copyright © 2016 ZhuRoger. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)closeView:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
